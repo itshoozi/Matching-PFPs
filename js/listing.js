2ec921bbc1ec1df68296818384a733024d697ecb
Jeremy Pringle <39378302+itshoozi@users.noreply.github.com>
2022-03-16 11:32:21 -0500

    Update listing.js

--- js/listing.js
+++ js/listing.js
@@ -1,4 +1,4 @@
-var postLength = 6;
+var postLength = 7;
 
 // Put your JavaScript here
 
@@ -16,18 +16,24 @@ Array.prototype.shuffle = function () {
     return input;
   }
   
-  let tempArray = [1, 2, 3, 4, 5, 6];
+  let tempArray = [1, 2, 3, 4, 5, 6, 1001];
   tempArray.shuffle();
 
 
   
   // and the result is...
 
-for (let e = 1; e < postLength; e++) {
-  let i = tempArray[e];  
+for (let e = 0; e < postLength; e++) {
+  let i = tempArray[e];
+  let fileType = "jpg";
+  if( i > 1000) {
+    fileType = 'gif';
+  } else {
+    fileType = 'jpg';
+  }
     document.getElementById('content').innerHTML += '<div class="grid grid-cols-2 gap-4 max-w-lg mx-auto">' +
     '<a href="images/' + [i] + '_left.jpg" download class="relative">' +
-    '<img id="'+ [i] + '" class="duration-300 mx-auto rounded-3xl shadow-lg aspect-square peer min-w-full" src="images/' + i + '_left.jpg" />' +
+    '<img id="'+ [i] + '" class="duration-300 mx-auto rounded-3xl shadow-lg aspect-square peer min-w-full" src="images/' + i + '_left.' + fileType + '" />' +
       '<div class="bg-transparent h-full justify-center w-full flex peer-hover:opacity-100 hover:opacity-100 jj opacity-0 backdrop-blur-sm absolute top-0 rounded-3xl">' +
         '<svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 self-center text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">' +
           '<path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />' +
@@ -35,7 +41,7 @@ for (let e = 1; e < postLength; e++) {
       '</div>' +
     '</a>' +
     '<a href="images/' + [i] + '_right.jpg" download class="relative">' +
-      '<img id="right" class="duration-300 mx-auto rounded-3xl shadow-lg aspect-square peer" src="images/' + i + '_right.jpg" />' +
+      '<img id="right" class="duration-300 mx-auto rounded-3xl shadow-lg aspect-square peer" src="images/' + i + '_right.' + fileType + '" />' +
       '<div class="bg-transparent h-full justify-center w-full flex peer-hover:opacity-100 hover:opacity-100 jj opacity-0 backdrop-blur-sm absolute top-0 rounded-3xl">' +
         '<svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 self-center text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">' +
           '<path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />' +
