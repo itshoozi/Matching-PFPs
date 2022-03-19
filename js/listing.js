06bb522a3f5de8398747dccba0d18df0f69da292
pringle143 <39378302+pringle143@users.noreply.github.com>
2022-03-19 13:35:39 -0500

    major changes
    added more pics, fixed load more button, random page now loads gifs aswell

--- js/listing.js
+++ js/listing.js
@@ -1,4 +1,7 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 1001, 1002, 1003];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 
+  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 
+  52, 1001, 1002, 1003, 1004, 1005, 1006, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015, 1016, 1018, 1019, 1020, 1021];
+let alt = ["Matching profile pictures for discord", "Matching anime profile pictures", "Matching profile pictures for couples", "Matching profile pictures for friends"];
 let currentIndex = 0;
 const button = document.getElementById("butt")
 
@@ -23,7 +26,7 @@ Array.prototype.shuffle = function () {
 
 
   // and the result is...
-  function load(amount=8) {
+  function ffff(amount=8) {
     if(currentIndex == arr.length-1) return;
     const newIndex = Math.min(currentIndex+amount+1,arr.length)
     for (let e = currentIndex; e < newIndex;e++) {
@@ -36,7 +39,7 @@ Array.prototype.shuffle = function () {
     }
       document.getElementById('content').innerHTML += '<div id="'+ [i] + '" class="grid grid-cols-2 gap-4 max-w-lg mx-auto fadeIn">' +
       '<a href="images/' + [i] + '_left.' + fileType + '" download class="relative">' +
-      '<img id="'+ [i] + '_left" class="duration-300 mx-auto rounded-3xl shadow-lg aspect-square peer min-w-full" src="images/' + i + '_left.' + fileType + '" />' +
+      '<img id="'+ [i] + '_left" class="duration-300 mx-auto rounded-3xl shadow-lg aspect-square peer min-w-full" src="images/' + i + '_left.' + fileType + '" alt="matching profile pictures" />' +
         '<div class="bg-transparent h-full justify-center w-full flex peer-hover:opacity-100 hover:opacity-100 jj opacity-0 backdrop-blur-sm absolute top-0 rounded-3xl">' +
           '<svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 self-center text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">' +
             '<path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />' +
@@ -44,7 +47,7 @@ Array.prototype.shuffle = function () {
         '</div>' +
       '</a>' +
       '<a href="images/' + [i] + '_right.' + fileType + '" download class="relative">' +
-        '<img id="'+ [i] + '_right" class="duration-300 mx-auto rounded-3xl shadow-lg aspect-square peer" src="images/' + i + '_right.' + fileType + '" />' +
+        '<img id="'+ [i] + '_right" class="duration-300 mx-auto rounded-3xl shadow-lg aspect-square peer" src="images/' + i + '_right.' + fileType + '" alt="matching profile pictures" />' +
         '<div class="bg-transparent h-full justify-center w-full flex peer-hover:opacity-100 hover:opacity-100 jj opacity-0 backdrop-blur-sm absolute top-0 rounded-3xl">' +
           '<svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 self-center text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">' +
             '<path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />' +
@@ -54,7 +57,17 @@ Array.prototype.shuffle = function () {
     '</div>';
 
       currentIndex = newIndex
+      setTimeout(function () {
+        window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+      }, 120);
+    }
   }
+
+  function load() {
+    ffff();
+    if(currentIndex == arr.length) {
+      butt.classList.add("hidden");
+    }
   }
 
 window.addEventListener("load", ()=>{load()})
